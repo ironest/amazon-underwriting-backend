cc5ac474020b97f704c6fe3c1bb5812d6ca77c62
Ricky <ironest@gmail.com>
2020-01-21 17:02:45 +1100

    Fixed Info Routes

--- routes/info_routes.js
+++ routes/info_routes.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const LinkController = require("../controllers/info_controller");
+const InfoController = require("../controllers/info_controller");
 
-router.get("/", LinkController.index);
+router.get("/", InfoController.index);
 
-/*
-    Not sure how safe is to be provide below endpoints
-    as there MUST be one and only one entry AT ANY TIME
-*/
+router.patch("/", InfoController.update);
 
-// router.post("/", LinkController.create);
-// router.get("/new", LinkController.make);
-// router.delete("/", LinkController.destroy);
-
-router.patch("/", LinkController.update);
-
-router.put("/", LinkController.update);
+router.put("/", InfoController.update);
 
 module.exports = router;
