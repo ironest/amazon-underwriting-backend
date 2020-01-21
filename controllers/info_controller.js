5b0f4b010846e9dab447e4e74c2c84d7e465c746
Ricky <ironest@gmail.com>
2020-01-21 17:03:09 +1100

    Implemented index and update Info-Controller

--- controllers/info_controller.js
+++ controllers/info_controller.js
@@ -1,27 +1,26 @@
-function create(req, res) {
-    //logic for creating a resource
+const InfoModel = require('../database/models/info_model')
+
+async function index(req, res) {
+  //showed a list of all the resources
+
+  let info = await InfoModel.find()  // Extracting all the info from the DB
+  res.json( info[0] )                   // Returns the data
+
 }
 
-function index(req, res) {
-    //showed a list of all the resources
-}
+async function update(req, res) {
+  //updates the resource
 
-function destroy(req, res) {
-    //deletes the resource
-}
+  let { id } = req.params // Destructure off the id off req.params
 
-function show(req, res) {
-    //show a single resource
-}
+  let { footer_info, childcare_info, hbb_info, accident_info, contact_info } = req.body // Destructure name, bio and gender off req.body
 
-function update(req, res) {
-    //updates the resource
+  await InfoModel.findByIdAndUpdate(id, { footer_info, childcare_info, hbb_info, accident_info, contact_info }) // Update the author. Find by the id and then update with the new name, bio and gender
+  res.redirect(`/info`);
+
 }
 
 module.exports = {
-    create,
-    index,
-    destroy,
-    show,
-    update
+  index,
+  update
 }
