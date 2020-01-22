2e078ea4efaa9f6532acefafde4958ffc569824e
Ricky <ironest@gmail.com>
2020-01-23 00:56:46 +1100

    Updated error handling on Info endpoints

--- controllers/info_controller.js
+++ controllers/info_controller.js
@@ -3,21 +3,44 @@ const InfoModel = require('../database/models/info_model')
 async function index(req, res) {
   //showed a list of all the resources
 
-  let info = await InfoModel.find()  // Extracting all the info from the DB
-  res.json( info[0] )                   // Returns the data
+  let infoDoc;
+  try {
+    infoDoc = await InfoModel.findOne();  // Extracting all the info from the DB
+  } catch (error) {
+    return res.status(500).json({ error : error.message });
+  }
+
+  if (!infoDoc) { 
+    return res.status(404).json({ error : "Information doesn't exist" });
+  };
+
+  res.json( infoDoc );  // Returns the data
 
 }
 
 async function update(req, res) {
   //updates the resource
 
-  let { id } = req.params // Destructure off the id off req.params
-
   let { footer_info, childcare_info, hbb_info, accident_info, contact_info } = req.body // Destructure infos off req.body
 
-  await InfoModel.findByIdAndUpdate(id, { footer_info, childcare_info, hbb_info, accident_info, contact_info }) // Update the info fields.
-  res.redirect(`/info`);
+  let infoDoc;
 
+  try {
+    infoDoc = await InfoModel.updateOne(
+      {}, // what to match 
+      { footer_info, childcare_info, hbb_info, accident_info, contact_info }, // new data
+      { omitUndefined: true } // true = ignore undefined data
+    )
+  } catch (error) {
+    return res.status(500).json({ error : error.message });
+  }
+
+  if (!infoDoc) { 
+    return res.status(404).json({ error : "Information doesn't exist" });
+  };
+
+  res.redirect(`/info`);
+  
 }
 
 module.exports = {
