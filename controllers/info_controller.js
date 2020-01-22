93106a74bae0f57ac2f5b27bdaf15e84313ab7ff
Ricky <ironest@gmail.com>
2020-01-22 16:41:00 +1100

    Fixed controllers references

--- controllers/info_controller.js
+++ controllers/info_controller.js
@@ -1,34 +1,26 @@
-function create(req, res) {
-    //logic for creating a resource
+const InfoModel = require('../database/models/info_model')
+
+async function index(req, res) {
+  //showed a list of all the resources
+
+  let info = await InfoModel.find()  // Extracting all the info from the DB
+  res.json( info[0] )                   // Returns the data
+
 }
 
-function index(req, res) {
-    //showed a list of all the resources
-}
+async function update(req, res) {
+  //updates the resource
 
-function make(req, res) {
-    //shows the form to create the resource
-}
-
-function destroy(req, res) {
-    //deletes the resource
-}
+  let { id } = req.params // Destructure off the id off req.params
 
   let { footer_info, childcare_info, hbb_info, accident_info, contact_info } = req.body // Destructure infos off req.body
 
   await InfoModel.findByIdAndUpdate(id, { footer_info, childcare_info, hbb_info, accident_info, contact_info }) // Update the info fields.
   res.redirect(`/info`);
 
-function edit(req, res) {
-    //shows the form to edit the resource
 }
 
 module.exports = {
-    create,
-    index,
-    make,
-    destroy,
-    show,
-    update,
-    edit
+  index,
+  update
 }
--- routes/link_routes.js
+++ routes/link_routes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const LinkController = require("../controllers/link_controller");
 
-// router.get("/", LinkController.index);
-
 router.post("/", LinkController.create);
 
 router.delete("/:id", LinkController.destroy);
@@ -13,8 +11,6 @@ router.patch("/:id", LinkController.update);
 router.put("/:id", LinkController.update);
 
 router.get("/:id", LinkController.show);
-
-router.get("/:page/edit", LinkController.edit);
 
 module.exports = router;
 
