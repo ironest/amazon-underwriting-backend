a833ea394157ce08332ad5a52d591e8f5335869d
Ricky <ironest@gmail.com>
2020-01-23 11:53:30 +1100

    Added extra options to Mongoose.connect to remove warnings

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,19 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
+/*
+ Current Server Discovery and Monitoring engine is deprecated,
+ and will be removed in a future version. To use the new Server Discover
+ and Monitoring engine, pass option { useUnifiedTopology: true }
+ to the MongoClient constructor.
+*/
+
+mongoose.connect(
+  process.env.DB_HOST,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
 mongoose.Promise = global.Promise;
 mongoose.connection.on("error", console.log);
