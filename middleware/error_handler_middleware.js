b57d6f7b5898b168e7f11f935ddbbc70da9aadd8
Ricky <ironest@gmail.com>
2020-01-29 12:51:26 +1100

    Added json response for specific errors

--- middleware/error_handler_middleware.js
+++ middleware/error_handler_middleware.js
@@ -1,6 +1,17 @@
+const { isCelebrate } = require("celebrate");
+
 module.exports = function(err, req, res, next) {
+
     if (err && err.name === "HTTPError") {
         return res.status(err.statusCode).send(err.message);
+    }
+
+    if(err && err.name === "Passport"){
+        return res.status(401).json( { error: err.message } );
+    }
+
+    if(err && isCelebrate(err)){
+        return res.status(400).json( { error: err.message } );
     }
 
     return next(err);
