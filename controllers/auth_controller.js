5712828c5c4c299763e972a6c03f6f0987912cdb
Ricky <ironest@gmail.com>
2020-01-29 15:29:03 +1100

    Bug Fix: user variable not defined

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,7 +1,7 @@
 const JWTService = require("./../services/jwt_service");
 
 function loginCreate(req, res) {
-  const token = JWTService.generateToken(user);
+  const token = JWTService.generateToken(req.user);
   res.json(token);
 }
 
