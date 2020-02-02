df1195673f0be5abe5c3d38a8ac10f67095b9e93
Ricky <ironest@gmail.com>
2020-02-03 00:44:54 +1100

    Fixed token returned object

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -2,7 +2,7 @@ const JWTService = require("./../services/jwt_service");
 
 function loginCreate(req, res) {
   const token = JWTService.generateToken(req.user);
-  res.json(token);
+  res.json({token});
 }
 
 function logout(req, res) {
