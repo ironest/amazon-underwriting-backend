662977184506d47ed4897684a3ad39459ac27cd5
Ricky <ironest@gmail.com>
2020-02-05 10:49:57 +1100

    Added Multer to Newsletter upload file

--- routes/news_routes.js
+++ routes/news_routes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
+const multer = require('multer');
 const NewsController = require("../controllers/news_controller");
 
 router.get("/", NewsController.index);
 
-router.post("/", passport.authenticate('jwt', {session: false}), NewsController.create);
+router.post("/",
+            passport.authenticate('jwt', {session: false}),
+            multer({ dest: 'temp/', limits: { fieldSize: 8 * 1024 * 1024, fileSize: 8 * 1024 * 1024 } }).single('file'),
+            NewsController.create);
 
 router.delete("/:id", passport.authenticate('jwt', {session: false}), NewsController.destroy);
 
